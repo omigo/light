730bde89d21bb2e4c15d8cba4c4225d5f8dad94b
Arstd <shangxuejin@gmail.com>
2017-03-29 12:22:27 +0800

    many int type to db time

--- light/time.go
+++ light/time.go
@@ -4,19 +4,54 @@ import (
 	"database/sql"
 	"database/sql/driver"
 	"time"
+
+	"github.com/arstd/log"
 )
+
+// ugly code....
 
 func Time(a interface{}) interface {
 	driver.Valuer
 	sql.Scanner
 } {
-
 	switch v := a.(type) {
 	case uint32:
-		return &TimeWapper{Time: time.Unix(int64(v), 0), Uint32: &v, Valid: v != 0}
+		if v == 0 {
+			return nil
+		}
+		return &TimeWapper{Uint32: &v, Time: time.Unix(int64(v), 0), Valid: true}
+
+	case int32:
+		if v == 0 {
+			return nil
+		}
+		return &TimeWapper{Int32: &v, Time: time.Unix(int64(v), 0), Valid: true}
+	case int:
+		if v == 0 {
+			return nil
+		}
+		return &TimeWapper{Int: &v, Time: time.Unix(int64(v), 0), Valid: true}
+	case uint64:
+		if v == 0 {
+			return nil
+		}
+		return &TimeWapper{Uint64: &v, Time: time.Unix(int64(v), 0), Valid: true}
+	case int64:
+		if v == 0 {
+			return nil
+		}
+		return &TimeWapper{Int64: &v, Time: time.Unix(v, 0), Valid: true}
 
 	case *uint32:
-		return &TimeWapper{Time: time.Unix(int64(*v), 0), Uint32: v, Valid: *v != 0}
+		return &TimeWapper{Uint32: v}
+	case *int32:
+		return &TimeWapper{Int32: v}
+	case *int:
+		return &TimeWapper{Int: v}
+	case *uint64:
+		return &TimeWapper{Uint64: v}
+	case *int64:
+		return &TimeWapper{Int64: v}
 
 	default:
 		panic("type not implemented")
@@ -25,8 +60,13 @@ func Time(a interface{}) interface {
 
 type TimeWapper struct {
 	Uint32 *uint32
-	Time   time.Time
-	Valid  bool
+	Int32  *int32
+	Int    *int
+	Uint64 *uint64
+	Int64  *int64
+
+	Time  time.Time
+	Valid bool
 }
 
 func (b TimeWapper) Value() (driver.Value, error) {
@@ -37,9 +77,25 @@ func (b TimeWapper) Value() (driver.Value, error) {
 }
 
 func (b *TimeWapper) Scan(src interface{}) error {
+	log.Warn(src)
+	if src == nil {
+		return nil
+	}
+
 	b.Time, b.Valid = src.(time.Time)
 	if b.Valid {
-		*b.Uint32 = uint32(b.Time.Unix())
+		switch {
+		case b.Uint32 != nil:
+			*b.Uint32 = uint32(b.Time.Unix())
+		case b.Int32 != nil:
+			*b.Int32 = int32(b.Time.Unix())
+		case b.Int != nil:
+			*b.Int = int(b.Time.Unix())
+		case b.Uint64 != nil:
+			*b.Uint64 = uint64(b.Time.Unix())
+		case b.Int64 != nil:
+			*b.Int64 = b.Time.Unix()
+		}
 	}
 	return nil
 }
