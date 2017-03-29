df513e209b30f78be62966f019cead48a35de026
Arstd <shangxuejin@gmail.com>
2017-03-29 18:20:20 +0800

    remove context version error

--- light/execer.go
+++ light/execer.go
@@ -1,17 +1,12 @@
 package light
 
 import (
-	"context"
 	"database/sql"
 )
 
 type Execer interface {
 	Exec(query string, args ...interface{}) (sql.Result, error)
-	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	Prepare(query string) (*sql.Stmt, error)
-	PrepareContext(ctx context.Context, query string) (*sql.Stmt, error)
 	Query(query string, args ...interface{}) (*sql.Rows, error)
-	QueryContext(ctx context.Context, query string, args ...interface{}) (*sql.Rows, error)
 	QueryRow(query string, args ...interface{}) *sql.Row
-	QueryRowContext(ctx context.Context, query string, args ...interface{}) *sql.Row
 }
