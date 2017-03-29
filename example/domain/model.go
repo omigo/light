795a8dfa1fbdfaf1db6470dd45a410f8d4efd44d
Arstd <shangxuejin@gmail.com>
2017-03-29 12:24:49 +0800

    remove unused sql

--- example/domain/model.go
+++ example/domain/model.go
@@ -25,25 +25,3 @@ type Model struct {
 	StructSlice []*Model
 	Uint32      uint32
 }
-
-/*
-drop table if exists models;
-create table models (
-	id serial primary key,
-	name text not null,
-	flag bool not null default false,
-	score decimal(3,1) not null default 0.0,
-
-	map jsonb not null default '{}',
-	time timestamptz not null default now(),
-	xarray text[] not null,
-	slice text[] not null,
-
-	status smallint not null default 0,
-	state text not null default '',
-
-	pointer jsonb not null default '{}',
-	struct_slice jsonb not null default '[]',
-	uint32 bigint not null default 0
-)
-*/
