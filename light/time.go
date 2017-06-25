9710f4ae73f23937ea449a029658058375a998f6
Arstd <shangxuejin@gmail.com>
2017-06-25 11:36:36 +0800

    update light time parse

--- light/time.go
+++ light/time.go
@@ -12,52 +12,68 @@ func Time(a interface{}) interface {
 } {
 	switch v := a.(type) {
 	case uint32:
-		return &TimeWapper{Uint32: &v, Time: time.Unix(int64(v), 0), Valid: true}
+		return &TimeWapper{Uint32: &v, Time: time.Unix(int64(v), 0), Valid: v == 0}
 	case int32:
-		return &TimeWapper{Int32: &v, Time: time.Unix(int64(v), 0), Valid: true}
+		return &TimeWapper{Int32: &v, Time: time.Unix(int64(v), 0), Valid: v == 0}
 	case int:
-		return &TimeWapper{Int: &v, Time: time.Unix(int64(v), 0), Valid: true}
+		return &TimeWapper{Int: &v, Time: time.Unix(int64(v), 0), Valid: v == 0}
 	case uint64:
-		return &TimeWapper{Uint64: &v, Time: time.Unix(int64(v), 0), Valid: true}
+		return &TimeWapper{Uint64: &v, Time: time.Unix(int64(v), 0), Valid: v == 0}
 	case int64:
-		return &TimeWapper{Int64: &v, Time: time.Unix(v, 0), Valid: true}
+		return &TimeWapper{Int64: &v, Time: time.Unix(v, 0), Valid: v == 0}
 
 	case *uint32:
-		return &TimeWapper{Uint32: v}
+		if v == (*uint32)(nil) {
+			v = new(uint32)
+			return &TimeWapper{Uint32: v, Valid: false}
+		}
+		return &TimeWapper{Uint32: v, Time: time.Unix(int64(*v), 0), Valid: true}
 	case *int32:
-		return &TimeWapper{Int32: v}
+		if v == (*int32)(nil) {
+			return &TimeWapper{Valid: false}
+		}
+		return &TimeWapper{Int32: v, Time: time.Unix(int64(*v), 0), Valid: true}
 	case *int:
-		return &TimeWapper{Int: v}
+		if v == (*int)(nil) {
+			return &TimeWapper{Valid: false}
+		}
+		return &TimeWapper{Int: v, Time: time.Unix(int64(*v), 0), Valid: true}
 	case *uint64:
-		return &TimeWapper{Uint64: v}
+		if v == (*uint64)(nil) {
+			return &TimeWapper{Valid: false}
+		}
+		return &TimeWapper{Uint64: v, Time: time.Unix(int64(*v), 0), Valid: true}
 	case *int64:
-		return &TimeWapper{Int64: v}
+		if v == (*int64)(nil) {
+			return &TimeWapper{Valid: false}
+		}
+		return &TimeWapper{Int64: v, Time: time.Unix(*v, 0), Valid: true}
 
 	case **uint32:
 		if *v == (*uint32)(nil) {
 			*v = new(uint32)
 		}
-		return &TimeWapper{Uint32: *v}
+		return &TimeWapper{Uint32: *v, Time: time.Unix(int64(**v), 0), Valid: **v == 0}
 	case **int32:
 		if *v == (*int32)(nil) {
 			*v = new(int32)
 		}
-		return &TimeWapper{Int32: *v}
+		return &TimeWapper{Int32: *v, Time: time.Unix(int64(**v), 0), Valid: **v == 0}
 	case **int:
 		if *v == (*int)(nil) {
 			*v = new(int)
 		}
-		return &TimeWapper{Int: *v}
+		return &TimeWapper{Int: *v, Time: time.Unix(int64(**v), 0), Valid: **v == 0}
 	case **uint64:
 		if *v == (*uint64)(nil) {
 			*v = new(uint64)
 		}
-		return &TimeWapper{Uint64: *v}
+		return &TimeWapper{Uint64: *v, Time: time.Unix(int64(**v), 0), Valid: **v == 0}
 	case **int64:
 		if *v == (*int64)(nil) {
 			*v = new(int64)
 		}
-		return &TimeWapper{Int64: *v}
+		return &TimeWapper{Int64: *v, Time: time.Unix(**v, 0), Valid: **v == 0}
 	default:
 		panic("type not implemented")
 	}
@@ -78,6 +94,7 @@ func (b TimeWapper) Value() (driver.Value, error) {
 	if !b.Valid {
 		return nil, nil
 	}
+
 	return b.Time, nil
 }
 
